5bf1de618ac42d632ca9cfb546f07cceee8e4f6b
Eric Woroshow <eric@ericw.ca>
2013-02-15 23:07:46 -0800

    Iterated on the CPU.

--- cpu.go
+++ cpu.go
@@ -14,7 +14,7 @@ type Cpu struct {
 
 	// Behavior of the current instruction
 	pageCrossed bool
-	tookBranch  bool
+	branchTaken bool
 }
 
 const (
@@ -40,6 +40,43 @@ const (
 	InterruptReset
 )
 
+func (cpu *Cpu) Reset() {
+	cpu.a = 0
+	cpu.x = 0
+	cpu.y = 0
+	cpu.sp = 0xfd
+	cpu.pc = 0x34
+	cpu.flags = 0
+
+	cpu.interrupt = InterruptNone
+}
+
+func (cpu *Cpu) Step() int {
+	opcode := cpu.loadAndIncPc()
+	instruction, ok := instructions[opcode]
+	if !ok {
+		panic("Unimplemented/illegal instruction")
+	}
+	instruction.fn(cpu, instruction.addr)
+
+	cycles := instruction.cycles
+	if cpu.pageCrossed && instruction.hasPageCyclePenalty {
+		cycles++
+		cpu.pageCrossed = false
+	}
+	if cpu.branchTaken && instruction.hasBranchCyclePenalty {
+		cycles++
+		cpu.branchTaken = false
+	}
+	return cycles
+}
+
+func (cpu *Cpu) loadAndIncPc() uint8 {
+	val := Mem.Load(cpu.pc)
+	cpu.pc++
+	return val
+}
+
 // Addressing modes
 func immediate(cpu *Cpu) uint16 {
 	cpu.pc++
@@ -61,49 +98,53 @@ func zeroPageY(cpu *Cpu) uint16 {
 func absolute(cpu *Cpu) uint16 {
 	lowByte := cpu.loadAndIncPc()
 	highByte := cpu.loadAndIncPc()
-	return toU16(lowByte, highByte)
+	return makeWord(lowByte, highByte)
 }
 
 func absoluteX(cpu *Cpu) uint16 {
-  return indexed(cpu, absolute(cpu), cpu.x)
+	return indexed(cpu, absolute(cpu), cpu.x)
 }
 
 func absoluteY(cpu *Cpu) uint16 {
-  return indexed(cpu, absolute(cpu), cpu.y)
+	return indexed(cpu, absolute(cpu), cpu.y)
 }
 
 func indirect(cpu *Cpu) uint16 {
-	lowbIndirect := cpu.loadAndIncPc()
-	highbIndirect := cpu.loadAndIncPc()
-	lowb := Mem.Load(toU16(lowbIndirect, highbIndirect))
-	highb := Mem.Load(toU16(lowbIndirect+1, highbIndirect))
-	return toU16(lowb, highb)
+	lowByteInd := cpu.loadAndIncPc()
+	highByteInd := cpu.loadAndIncPc()
+	lowByte := Mem.Load(makeWord(lowByteInd, highByteInd))
+	highByte := Mem.Load(makeWord(lowByteInd+1, highByteInd))
+	return makeWord(lowByte, highByte)
 }
 
 func indexedIndirect(cpu *Cpu) uint16 {
 	addr := cpu.loadAndIncPc() + cpu.x
 	lowByte := Mem.Load(uint16(addr))
 	highByte := Mem.Load(uint16(addr + 1))
-	return toU16(lowByte, highByte)
+	return makeWord(lowByte, highByte)
 }
 
 func indirectIndexed(cpu *Cpu) uint16 {
-	zeroPageAddress := cpu.loadAndIncPc()
-	lowByte := Mem.Load(uint16(zeroPageAddress))
-	highByte := Mem.Load(uint16(zeroPageAddress + 1))
-  return indexed(cpu, toU16(lowByte, highByte), cpu.y)
+	zeroPageAddr := cpu.loadAndIncPc()
+	lowByte := Mem.Load(uint16(zeroPageAddr))
+	highByte := Mem.Load(uint16(zeroPageAddr + 1))
+	return indexed(cpu, makeWord(lowByte, highByte), cpu.y)
 }
 
 func implied(cpu *Cpu) uint16 {
-  panic("Implied addressing should never be invoked")
+	panic("Implied addressing should never be invoked")
 }
 
 func indexed(cpu *Cpu, base uint16, index uint8) uint16 {
-  indexed := base + uint16(index)
-  if base&0xff00 != indexed&0xff00 {
-    cpu.pageCrossed = true
-  }
+	indexed := base + uint16(index)
+	if base&0xff00 != indexed&0xff00 {
+		cpu.pageCrossed = true
+	}
 	return indexed
+}
+
+func makeWord(lowb, highb uint8) uint16 {
+	return uint16(highb)<<8 | uint16(lowb)
 }
 
 // Instructions
@@ -116,13 +157,13 @@ type Instruction struct {
 
 	// Number of cycles taken by this instruction, including extra cycles if the as
 	// address crosses a page boundary or a branch is taken
-	cycles                   int8
-	hasPageCrossCyclePenalty bool
-	hasBranchCyclePenalty    bool
+	cycles                int
+	hasPageCyclePenalty   bool
+	hasBranchCyclePenalty bool
 }
 
 var instructions = map[uint8]Instruction{
-  // LDA
+	// LDA
 	0xa9: {fn: lda, addr: immediate},
 	0xa5: {fn: lda, addr: zeroPage},
 	0xb5: {fn: lda, addr: zeroPageX},
@@ -131,13 +172,13 @@ var instructions = map[uint8]Instruction{
 	0xb9: {fn: lda, addr: absoluteY},
 	0xa1: {fn: lda, addr: indexedIndirect},
 	0xb1: {fn: lda, addr: indirectIndexed},
-  // LDX
+	// LDX
 	0xa2: {fn: ldx, addr: immediate},
 	0xa6: {fn: ldx, addr: zeroPage},
 	0xb6: {fn: ldx, addr: zeroPageY},
 	0xae: {fn: ldx, addr: absolute},
 	0xbe: {fn: ldx, addr: absoluteY},
-  // LDY
+	// LDY
 	0xa0: {fn: ldy, addr: immediate},
 	0xa4: {fn: ldy, addr: zeroPage},
 	0xb4: {fn: ldy, addr: zeroPageX},
@@ -145,86 +186,16 @@ var instructions = map[uint8]Instruction{
 	0xbc: {fn: ldy, addr: absoluteX},
 }
 
-func lda(cpu *Cpu, addr AddressFn) { cpu.a = cpu.setNZ(Mem.Load(addr(cpu))) }
-func ldx(cpu *Cpu, addr AddressFn) { cpu.x = cpu.setNZ(Mem.Load(addr(cpu))) }
-func ldy(cpu *Cpu, addr AddressFn) { cpu.y = cpu.setNZ(Mem.Load(addr(cpu))) }
-
-func (cpu *Cpu) Reset() {
-	cpu.a = 0
-	cpu.x = 0
-	cpu.y = 0
-	cpu.sp = 0xfd
-	cpu.pc = 0x34
-	cpu.flags = 0
-
-	cpu.interrupt = InterruptNone
+func lda(cpu *Cpu, addr AddressFn) {
+	cpu.a = cpu.setNZ(Mem.Load(addr(cpu)))
 }
 
-func (cpu *Cpu) Step() int {
-	opcode := cpu.loadAndIncPc()
-  instruction, ok := instructions[opcode]
-  if !ok {
-    panic("Unimplemented/illegal instruction")
-  }
-  instruction.fn(cpu, instruction.addr)
-
-	return 0
+func ldx(cpu *Cpu, addr AddressFn) {
+	cpu.x = cpu.setNZ(Mem.Load(addr(cpu)))
 }
 
-func (cpu *Cpu) loadAndIncPc() uint8 {
-	val := Mem.Load(cpu.pc)
-	cpu.pc++
-	return val
-}
-
-// Addressing
-func (cpu *Cpu) immediateAddress() uint16 {
-	cpu.pc++
-	return cpu.pc - 1
-}
-
-func (cpu *Cpu) zeroPageAddress() uint16 {
-	return uint16(cpu.loadAndIncPc())
-}
-
-func (cpu *Cpu) zeroPageIndexedAddress(index uint8) uint16 {
-	return uint16(cpu.loadAndIncPc() + index)
-}
-
-func (cpu *Cpu) absoluteAddress() uint16 {
-	lowb := cpu.loadAndIncPc()
-	highb := cpu.loadAndIncPc()
-	return toU16(lowb, highb)
-}
-
-func (cpu *Cpu) absoluteIndexedAddress(index uint8) uint16 {
-	return cpu.absoluteAddress() + uint16(index)
-}
-
-func (cpu *Cpu) indirectAddress() uint16 {
-	lowbIndirect := cpu.loadAndIncPc()
-	highbIndirect := cpu.loadAndIncPc()
-	lowb := Mem.Load(toU16(lowbIndirect, highbIndirect))
-	highb := Mem.Load(toU16(lowbIndirect+1, highbIndirect))
-	return toU16(lowb, highb)
-}
-
-func (cpu *Cpu) indexedIndirectAddress() uint16 {
-	addr := cpu.loadAndIncPc() + cpu.x
-	lowb := Mem.Load(uint16(addr))
-	highb := Mem.Load(uint16(addr + 1))
-	return toU16(lowb, highb)
-}
-
-func (cpu *Cpu) indirectIndexedAddress() uint16 {
-	addr := cpu.loadAndIncPc()
-	lowb := Mem.Load(uint16(addr))
-	highb := Mem.Load(uint16(addr + 1))
-	return toU16(lowb, highb) + uint16(cpu.y)
-}
-
-func toU16(lowb, highb uint8) uint16 {
-	return uint16(highb)<<8 | uint16(lowb)
+func ldy(cpu *Cpu, addr AddressFn) {
+	cpu.y = cpu.setNZ(Mem.Load(addr(cpu)))
 }
 
 // Flags
@@ -245,10 +216,3 @@ func (cpu *Cpu) setNZ(val uint8) uint8 {
 	cpu.setFlag(ZeroFlag, val == 0)
 	return val
 }
-
-// Loads
-func (cpu *Cpu) lda(addr uint16) { cpu.a = cpu.setNZ(Mem.Load(addr)) }
-func (cpu *Cpu) ldx(addr uint16) { cpu.x = cpu.setNZ(Mem.Load(addr)) }
-func (cpu *Cpu) ldy(addr uint16) { cpu.y = cpu.setNZ(Mem.Load(addr)) }
-
-// Stores
