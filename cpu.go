067455aedc253570b4a23b9069d1ddff335defa0
Eric Woroshow <eric@ericw.ca>
2013-02-16 00:00:37 -0800

    Iterated on the CPU.

--- cpu.go
+++ cpu.go
@@ -164,38 +164,116 @@ type Instruction struct {
 
 var instructions = map[uint8]Instruction{
 	// LDA
-	0xa9: {fn: lda, addr: immediate},
-	0xa5: {fn: lda, addr: zeroPage},
-	0xb5: {fn: lda, addr: zeroPageX},
-	0xad: {fn: lda, addr: absolute},
-	0xbd: {fn: lda, addr: absoluteX},
-	0xb9: {fn: lda, addr: absoluteY},
-	0xa1: {fn: lda, addr: indexedIndirect},
-	0xb1: {fn: lda, addr: indirectIndexed},
+	0xa9: {fn: lda, addr: immediate, cycles: 2},
+	0xa5: {fn: lda, addr: zeroPage, cycles: 3},
+	0xb5: {fn: lda, addr: zeroPageX, cycles: 4},
+	0xad: {fn: lda, addr: absolute, cycles: 4},
+	0xbd: {fn: lda, addr: absoluteX, cycles: 4, hasPageCyclePenalty: true},
+	0xb9: {fn: lda, addr: absoluteY, cycles: 4, hasPageCyclePenalty: true},
+	0xa1: {fn: lda, addr: indexedIndirect, cycles: 6},
+	0xb1: {fn: lda, addr: indirectIndexed, cycles: 5, hasPageCyclePenalty: true},
 	// LDX
-	0xa2: {fn: ldx, addr: immediate},
-	0xa6: {fn: ldx, addr: zeroPage},
-	0xb6: {fn: ldx, addr: zeroPageY},
-	0xae: {fn: ldx, addr: absolute},
-	0xbe: {fn: ldx, addr: absoluteY},
+	0xa2: {fn: ldx, addr: immediate, cycles: 2},
+	0xa6: {fn: ldx, addr: zeroPage, cycles: 3},
+	0xb6: {fn: ldx, addr: zeroPageY, cycles: 4},
+	0xae: {fn: ldx, addr: absolute, cycles: 4},
+	0xbe: {fn: ldx, addr: absoluteY, cycles: 4, hasPageCyclePenalty: true},
 	// LDY
-	0xa0: {fn: ldy, addr: immediate},
-	0xa4: {fn: ldy, addr: zeroPage},
-	0xb4: {fn: ldy, addr: zeroPageX},
-	0xac: {fn: ldy, addr: absolute},
-	0xbc: {fn: ldy, addr: absoluteX},
+	0xa0: {fn: ldy, addr: immediate, cycles: 2},
+	0xa4: {fn: ldy, addr: zeroPage, cycles: 3},
+	0xb4: {fn: ldy, addr: zeroPageX, cycles: 4},
+	0xac: {fn: ldy, addr: absolute, cycles: 4},
+	0xbc: {fn: ldy, addr: absoluteX, cycles: 4, hasPageCyclePenalty: true},
+	// STA
+	0x85: {fn: sta, addr: zeroPage, cycles: 3},
+	0x95: {fn: sta, addr: zeroPageX, cycles: 4},
+	0x8d: {fn: sta, addr: absolute, cycles: 4},
+	0x9d: {fn: sta, addr: absoluteX, cycles: 5},
+	0x99: {fn: sta, addr: absoluteY, cycles: 5},
+	0x81: {fn: sta, addr: indexedIndirect, cycles: 6},
+	0x91: {fn: sta, addr: indirectIndexed, cycles: 6},
+	// STX
+	0x86: {fn: stx, addr: zeroPage, cycles: 3},
+	0x96: {fn: stx, addr: zeroPageY, cycles: 4},
+	0x8e: {fn: stx, addr: absolute, cycles: 4},
+	0x84: {fn: sty, addr: zeroPage, cycles: 3},
+	0x94: {fn: sty, addr: zeroPageX, cycles: 4},
+	0x8c: {fn: sty, addr: absolute, cycles: 4},
+	// TAX, TAY, TXA, TYA, TSX, TXS
+	0xaa: {fn: tax, addr: implied, cycles: 2},
+	0xa8: {fn: tay, addr: implied, cycles: 2},
+	0x8a: {fn: txa, addr: implied, cycles: 2},
+	0x98: {fn: tya, addr: implied, cycles: 2},
+	0xba: {fn: tsx, addr: implied, cycles: 2},
+	0x9a: {fn: txs, addr: implied, cycles: 2},
+	// PHA, PHP, PLA, PLP
+	0x48: {fn: pha, addr: implied, cycles: 3},
+	0x08: {fn: php, addr: implied, cycles: 3},
+	0x68: {fn: pla, addr: implied, cycles: 4},
+	0x28: {fn: plp, addr: implied, cycles: 4},
+	// AND
+	0x29: {fn: and, addr: immediate, cycles: 2},
+	0x25: {fn: and, addr: zeroPage, cycles: 3},
+	0x35: {fn: and, addr: zeroPageX, cycles: 4},
+	0x2d: {fn: and, addr: absolute, cycles: 4},
+	0x3d: {fn: and, addr: absoluteX, cycles: 4, hasPageCyclePenalty: true},
+	0x39: {fn: and, addr: absoluteY, cycles: 4, hasPageCyclePenalty: true},
+	0x21: {fn: and, addr: indexedIndirect, cycles: 6},
+	0x31: {fn: and, addr: indirectIndexed, cycles: 5, hasPageCyclePenalty: true},
+	// EOR
+	0x49: {fn: eor, addr: immediate, cycles: 2},
+	0x45: {fn: eor, addr: zeroPage, cycles: 3},
+	0x55: {fn: eor, addr: zeroPageX, cycles: 4},
+	0x4d: {fn: eor, addr: absolute, cycles: 4},
+	0x5d: {fn: eor, addr: absoluteX, cycles: 4, hasPageCyclePenalty: true},
+	0x59: {fn: eor, addr: absoluteY, cycles: 4, hasPageCyclePenalty: true},
+	0x41: {fn: eor, addr: indexedIndirect, cycles: 6},
+	0x51: {fn: eor, addr: indirectIndexed, cycles: 5, hasPageCyclePenalty: true},
+	// ORA
+	0x09: {fn: ora, addr: immediate, cycles: 2},
+	0x05: {fn: ora, addr: zeroPage, cycles: 3},
+	0x15: {fn: ora, addr: zeroPageX, cycles: 4},
+	0x0d: {fn: ora, addr: absolute, cycles: 4},
+	0x1d: {fn: ora, addr: absoluteX, cycles: 4, hasPageCyclePenalty: true},
+	0x19: {fn: ora, addr: absoluteY, cycles: 4, hasPageCyclePenalty: true},
+	0x01: {fn: ora, addr: indexedIndirect, cycles: 6},
+	0x11: {fn: ora, addr: indirectIndexed, cycles: 5, hasPageCyclePenalty: true},
 }
 
-func lda(cpu *Cpu, addr AddressFn) {
-	cpu.a = cpu.setNZ(Mem.Load(addr(cpu)))
+func lda(cpu *Cpu, addr AddressFn) { cpu.a = cpu.setNZ(Mem.Load(addr(cpu))) }
+func ldx(cpu *Cpu, addr AddressFn) { cpu.x = cpu.setNZ(Mem.Load(addr(cpu))) }
+func ldy(cpu *Cpu, addr AddressFn) { cpu.y = cpu.setNZ(Mem.Load(addr(cpu))) }
+
+func sta(cpu *Cpu, addr AddressFn) { Mem.Store(addr(cpu), cpu.a) }
+func stx(cpu *Cpu, addr AddressFn) { Mem.Store(addr(cpu), cpu.x) }
+func sty(cpu *Cpu, addr AddressFn) { Mem.Store(addr(cpu), cpu.y) }
+
+func tax(cpu *Cpu, addr AddressFn) { cpu.x = cpu.setNZ(cpu.a) }
+func tay(cpu *Cpu, addr AddressFn) { cpu.y = cpu.setNZ(cpu.a) }
+func txa(cpu *Cpu, addr AddressFn) { cpu.a = cpu.setNZ(cpu.x) }
+func tya(cpu *Cpu, addr AddressFn) { cpu.a = cpu.setNZ(cpu.y) }
+func tsx(cpu *Cpu, addr AddressFn) { cpu.x = cpu.setNZ(cpu.sp) }
+func txs(cpu *Cpu, addr AddressFn) { cpu.sp = cpu.x }
+
+func pha(cpu *Cpu, addr AddressFn) { push(cpu, cpu.a) }
+func php(cpu *Cpu, addr AddressFn) { push(cpu, cpu.flags) }
+func pla(cpu *Cpu, addr AddressFn) { cpu.a = cpu.setNZ(pop(cpu)) }
+func plp(cpu *Cpu, addr AddressFn) { cpu.flags = pop(cpu) }
+
+func and(cpu *Cpu, addr AddressFn) { cpu.a = cpu.setNZ(cpu.a & Mem.Load(addr(cpu))) }
+func eor(cpu *Cpu, addr AddressFn) { cpu.a = cpu.setNZ(cpu.a ^ Mem.Load(addr(cpu))) }
+func ora(cpu *Cpu, addr AddressFn) { cpu.a = cpu.setNZ(cpu.a | Mem.Load(addr(cpu))) }
+
+func bit(cpu *Cpu, addr AddressFn) {}
+
+func push(cpu *Cpu, val uint8) {
+	Mem.Store(0x100+uint16(cpu.sp), val)
+	cpu.sp--
 }
 
-func ldx(cpu *Cpu, addr AddressFn) {
-	cpu.x = cpu.setNZ(Mem.Load(addr(cpu)))
-}
-
-func ldy(cpu *Cpu, addr AddressFn) {
-	cpu.y = cpu.setNZ(Mem.Load(addr(cpu)))
+func pop(cpu *Cpu) uint8 {
+	cpu.sp++
+	return Mem.Load(0x100 + uint16(cpu.sp))
 }
 
 // Flags
